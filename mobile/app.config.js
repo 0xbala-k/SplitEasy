71f348e0fd0ee597dcfa55338554234c6a94aeac
0xmuralik <krishna192reddy@gmail.com>
2026-04-23 22:03:51 -0700

    chore(mobile): add EAS project ID

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -9,6 +9,9 @@ module.exports = {
       workerBaseUrl: process.env.WORKER_BASE_URL ?? '',
       workerApiKey: process.env.WORKER_API_KEY ?? '',
       splitwiseClientId: process.env.SPLITWISE_CLIENT_ID ?? '',
+      eas: {
+        projectId: '3999203f-8dd2-4e33-a7a4-4cf3bdb6949c',
+      },
     },
   },
 };
